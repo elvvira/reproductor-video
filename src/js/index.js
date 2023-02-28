881bf0d41080edf5027bf0aa613f36bd49642bba
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-02-28 14:23:21 +0100

    fin

--- src/js/index.js
+++ src/js/index.js
@@ -11,9 +11,13 @@ const buttonBack = document.getElementById('video-back');
 const buttonFront = document.getElementById('video-front');
 const videoPause = document.getElementById('video-pause');
 const volume = document.getElementById('volume');
-const videoBar = document.getElementById('video-bar');
-const ball = document.getElementById('ball');
+// const videoBar = document.getElementById('video-bar');
+// const ball = document.getElementById('ball');
 const barDark = document.getElementById('bar-dark');
+const range = document.getElementById('range-volume');
+const inputRange = document.getElementById('input');
+const timeVideo = document.getElementById('time');
+const timeVideoFinal = document.getElementById('final-time');
 const rootStyles = document.documentElement.style;
 
 const barWidth = () => {
@@ -34,22 +38,62 @@ const videoBack = () => {
   const backTime = (video.currentTime -= 10);
   video.currentTime = backTime;
 };
+const videoFront = () => {
+  const frontTime = (video.currentTime += 10);
+  video.currentTime = frontTime;
+};
+const rangeVolume = value => {
+  console.dir(value);
+  video.volume = value * 0.1;
 
-const volumeVideo = () => {
-  console.dir(video.volume);
-  if (video.volume === 1) {
+  if (video.volume === 0) {
     volume.src = imageMute;
   } else {
     volume.src = imageVolume;
   }
 };
-volume.addEventListener('click', e => {
-  volumeVideo();
-});
-const videoFront = () => {
-  const frontTime = (video.currentTime += 10);
-  video.currentTime = frontTime;
+const volumeVideo = value => {
+  if (value.nextElementSibling.classList.value === 'range') {
+    range.classList.add('range--show');
+  } else {
+    range.classList.remove('range--show');
+  }
 };
+const formatSecondsCurrent = currentSeconds => {
+  const date = new Date(null);
+  date.setSeconds(currentSeconds);
+  const timeString = date.toISOString().slice(14, 19);
+  timeVideo.textContent = timeString;
+};
+
+const formatSeconds = segundosTotales => {
+  const date = new Date(null);
+  date.setSeconds(segundosTotales);
+  const timeString = date.toISOString().slice(14, 19);
+  timeVideoFinal.textContent = timeString;
+};
+
+const LS = localStorage;
+
+const saveDataInLocalStorage = data => {
+  LS.setItem('currentTime', data);
+};
+window.addEventListener('load', e => {
+  const getData = JSON.parse(LS.getItem('currentTime'));
+  console.log(getData);
+  video.currentTime = getData;
+});
+
+video.addEventListener('loadedmetadata', () => {
+  formatSeconds(video.duration);
+});
+inputRange.addEventListener('change', e => {
+  rangeVolume(e.target.value);
+});
+
+volume.addEventListener('click', e => {
+  volumeVideo(e.target);
+});
 
 buttonBack.addEventListener('click', e => {
   videoBack();
@@ -65,11 +109,19 @@ videoPause.addEventListener('click', e => {
 
 video.addEventListener('timeupdate', e => {
   barWidth();
+  formatSecondsCurrent(video.currentTime);
+  saveDataInLocalStorage(video.currentTime);
 });
 
 window.addEventListener('keyup', e => {
   if (e.code === 'Space') {
     playPause();
+  }
+  if (e.code === 'ArrowLeft') {
+    videoBack();
+  }
+  if (e.code === 'ArrowRight') {
+    videoFront();
   }
 });
 barDark.addEventListener('click', e => {
